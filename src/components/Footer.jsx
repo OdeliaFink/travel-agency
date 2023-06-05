06433757578af1ff5abf86c0b52ec7d9f40d5295
OdeliaFink <odelia.finkel@gmail.com>
2023-06-04 22:07:49 -0400

    refactor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,17 +30,6 @@ export default function Footer() {
               You can dream, create, design, and build the most wonderful place.
             </p>
           </div>
-          <ul>
-            <li>
-              <span> +0213 456 987 </span>
-            </li>
-            <li>
-              <span>kishansheth21@gmail.com</span>
-            </li>
-            <li>
-              <span>www.youtube.com</span>
-            </li>
-          </ul>
         </div>
         <div className="col">
           <h2>Quick Links</h2>
