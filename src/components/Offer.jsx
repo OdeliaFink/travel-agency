33b7eee5b58a52116fe93da6f3250d0fe1d56027
OdeliaFink <odelia.finkel@gmail.com>
2023-06-24 21:45:09 -0400

    refactor

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -52,7 +52,7 @@ const Section = styled.section`
   display: flex;
   gap: 5rem;
   margin-inline: 3rem;
-  padding-left: 3rem;
+  padding-left: 6rem;
 
   @media screen and (min-width: 280px) and (max-width: 1080px) {
     flex-direction: column;
